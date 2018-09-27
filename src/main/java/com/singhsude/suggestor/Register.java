651630f2c806ae7b953eb0bb31d523005812fe33
singhsude <sudeepchandla@gmail.com>
2018-09-27 07:36:35 +0530

    Add files via upload

--- src/main/java/com/singhsude/suggestor/Register.java
+++ src/main/java/com/singhsude/suggestor/Register.java
@@ -6,7 +6,7 @@ public class Register {
 	public String Result(String store) {
 	        try {   
 		        String absolutePath = System.getenv("OPENSHIFT_DATA_DIR");
-                        PrintWriter p = new PrintWriter(absolutePath + "register.txt");
+                        PrintWriter p = new PrintWriter(new FileOutputStream(absolutePath + "register.txt", false));
 			p.write(store + "\n\n");
 			p.close();
         	} catch (Exception e) {
