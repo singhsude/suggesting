b91ea648d26d3a65203f56c8a5eb368af9b72d77
singhsude <sudeepchandla@gmail.com>
2019-01-14 09:46:57 +0530

    Update Register.java

--- src/main/java/com/singhsude/suggestor/Register.java
+++ src/main/java/com/singhsude/suggestor/Register.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class Register {
 	public String Result(String store) {
 	        try {   
+			if(store.split("\n")[0].length() == 0 || store.split("\n")[1].length() == 0 || store.split("\n")[2].length() == 0 || store.split("\n")[3].length() == 0 || store.split("\n")[4].length() == 0)
+			{
+				return "false";
+			}
 		        String absolutePath = System.getenv("OPENSHIFT_DATA_DIR");
                         PrintWriter p = new PrintWriter(new FileOutputStream(absolutePath + "register.txt", true));
 			p.write(store + "\n\n");
