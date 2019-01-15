85f1467f76fa011c9affd1b0483a1a3761895da7
singhsude <sudeepchandla@gmail.com>
2019-01-15 18:40:12 +0530

    latest

--- src/main/java/com/singhsude/suggestor/Register.java
+++ src/main/java/com/singhsude/suggestor/Register.java
@@ -7,11 +7,11 @@ public class Register {
 	        try { 
 			if(store.split("\n")[0].length() == 0 || store.split("\n")[1].length() == 0 || store.split("\n")[2].length() == 0 || store.split("\n")[3].length() == 0 || store.split("\n")[4].length() == 0 || store.split("\n")[5].length() == 0)
 				return "false";
-
-		        String absolutePath = System.getenv("OPENSHIFT_DATA_DIR");
-                        PrintWriter p = new PrintWriter(new FileOutputStream(absolutePath + "register.txt", true));
-			p.write(store + "\n\n");
-			p.close();
+			Util.putString{store};
+		        //String absolutePath = System.getenv("OPENSHIFT_DATA_DIR");
+                        //PrintWriter p = new PrintWriter(new FileOutputStream(absolutePath + "register.txt", true));
+			//p.write(store + "\n\n");
+			//p.close();
         	} catch (Exception e) {
 			return "false";
 	        }
