343df6ce32f4633094bda523d6811ba9effdbb6f
singhsude <sudeepchandla@gmail.com>
2019-01-16 06:45:02 +0530

    latest

--- src/main/java/com/singhsude/suggestor/Suggestor.java
+++ src/main/java/com/singhsude/suggestor/Suggestor.java
@@ -90,7 +90,7 @@ public class Suggestor {
         result2 += "age: " + result + "<br>";
         
 	    if(isTrue)
-		    results += "would you like to go for:<br>"
+		    results += "would you like to go for:<br>";
 		else
 			return result2;
 	    
