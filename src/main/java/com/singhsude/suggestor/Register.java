04fa0a59dcbeae9b6e266fe678b9649d0f1a1225
singhsude <sudeepchandla@gmail.com>
2019-01-16 07:03:51 +0530

    latest

--- src/main/java/com/singhsude/suggestor/Register.java
+++ src/main/java/com/singhsude/suggestor/Register.java
@@ -7,6 +7,10 @@ public class Register {
 	        try { 
 			if(store.split("\n")[0].length() == 0 || store.split("\n")[1].length() == 0 || store.split("\n")[2].length() == 0 || store.split("\n")[3].length() == 0 || store.split("\n")[4].length() == 0 || store.split("\n")[5].length() == 0)
 				return "false";
+			
+			if(!store.split("\n")[5].toLowerCase().contains("http:"))
+				return false;
+			
 			Util.putString(store);
 		        //String absolutePath = System.getenv("OPENSHIFT_DATA_DIR");
                         //PrintWriter p = new PrintWriter(new FileOutputStream(absolutePath + "register.txt", true));
